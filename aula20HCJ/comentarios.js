81db3304876d3beb135e31f6c416555109e1cb01
saaahisa <ssiva.2012@hotmail.com>
2021-12-02 17:59:16 -0300

    react part 1,2

---
+++ aula20HCJ/comentarios.js
@@ -0,0 +1,11 @@
+/*<button class="btn btn-dark">Dark</button>
+<button class="btn btn-light">Light</button>
+<button class="btn btn-link">Link</button>
+<br><br>
+<!-- Tamanho de Botões -->
+<button class="btn btn-outline-secondary btn-lg">Botão Um</button>
+<button class="btn btn-primary btn-sm">Primary</button>
+<button class="btn btn-secondary"*/
+
+let query = "SELECT * FROM PRODUTO WHERE COD_PROD = '003'";
+let queryJ = "SELECT * FROM FUNCIONARIO WHERE NOME_FUNC = 'JOANA D''ARC'";
